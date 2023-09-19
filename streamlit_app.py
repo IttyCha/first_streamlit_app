c745a71edbc2cfe5f0fd56a1cc2bc53c80c7e466
IttyCha <144943703+IttyCha@users.noreply.github.com>
2023-09-19 15:36:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,8 @@ def get_fruityvice_data(this_fruit_choice):
 
 streamlit.header('Fruityvice Fruit Advice!')
 
+streamlit.stop() 
+
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
